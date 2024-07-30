48e90e264717e04407b02d5d3073db145c245145
Elif Sude Cesur <elifsudecesur4@gmail.com>
2024-07-30 11:00:27 +0300

    Initial commit

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -1,21 +1,22 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.TELEGRAM_TOKEN; // Çevresel değişken olarak token'ı kullanın
-const bot = new TelegramBot(token, {polling: true});
+const { Telegraf } = require('telegraf');
+const token = '7400783507:AAGC4XhT0uWy3qniGfO-TmoFkkQ1-dSDeO8'; // Doğrudan token'ı burada tanımlayın
+const bot = new Telegraf(token);
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Welcome! Use /showapp to view the React app.');
+bot.start((ctx) => {
+  ctx.reply('Welcome! Use /showapp to view the React app.');
 });
 
-bot.onText(/\/showapp/, (msg) => {
-  const chatId = msg.chat.id;
+bot.command('showapp', (ctx) => {
   const url = 'https://66a88ad70e4c3a0008a20e40--techirrapp.netlify.app/'; // Netlify üzerinde dağıttığınız React uygulamanızın URL'si
-  bot.sendMessage(chatId, `Here is the React app: ${url}`);
+  ctx.reply(`Here is the React app: ${url}`);
 });
 
 exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: 'Telegram bot is running!',
-  };
+  try {
+    await bot.handleUpdate(JSON.parse(event.body));
+    return { statusCode: 200, body: 'Update handled' };
+  } catch (err) {
+    console.error('Error handling update', err);
+    return { statusCode: 500, body: 'Internal Server Error' };
+  }
 };
