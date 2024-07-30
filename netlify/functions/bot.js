98e53028f53f5b94040752b230efda0f31e1ffc0
Elif Sude Cesur <elifsudecesur4@gmail.com>
2024-07-30 11:18:16 +0300

    Initial commit

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -1,22 +1,21 @@
 const { Telegraf } = require('telegraf');
-const token = '7400783507:AAGC4XhT0uWy3qniGfO-TmoFkkQ1-dSDeO8'; // Doğrudan token'ı burada tanımlayın
-const bot = new Telegraf(token);
+const BOT_TOKEN = '7400783507:AAGC4XhT0uWy3qniGfO-TmoFkkQ1-dSDeO8';
+const WEB_LINK = 'https://66a88ad70e4c3a0008a20e40--techirrapp.netlify.app/';
+
+const bot = new Telegraf(BOT_TOKEN);
 
 bot.start((ctx) => {
-  ctx.reply('Welcome! Use /showapp to view the React app.');
+  ctx.reply('Welcome', {
+    reply_markup: {
+      keyboard: [
+        [
+          { text: "web app", web_app: { url: WEB_LINK } }
+        ]
+      ],
+      resize_keyboard: true,  // To ensure the keyboard fits the screen size
+      one_time_keyboard: true // To hide the keyboard after use
+    }
+  });
 });
 
-bot.command('showapp', (ctx) => {
-  const url = 'https://66a88ad70e4c3a0008a20e40--techirrapp.netlify.app/'; // Netlify üzerinde dağıttığınız React uygulamanızın URL'si
-  ctx.reply(`Here is the React app: ${url}`);
-});
-
-exports.handler = async function(event, context) {
-  try {
-    await bot.handleUpdate(JSON.parse(event.body));
-    return { statusCode: 200, body: 'Update handled' };
-  } catch (err) {
-    console.error('Error handling update', err);
-    return { statusCode: 500, body: 'Internal Server Error' };
-  }
-};
+bot.launch();
