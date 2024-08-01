371e6f9d1eaaa7aba138da72f3ea65890f6ebc13
Elif Sude Cesur <elifsudecesur4@gmail.com>
2024-08-01 16:27:54 +0300

    Initial commit

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -1,9 +1,10 @@
 const { Telegraf } = require('telegraf');
-const BOT_TOKEN = '7309348405:AAEJmW2iw5zLkbhuEFg0kMlQIxyFpcEaZ0M'; // Doğrudan kodda token tanımlayın
+const BOT_TOKEN = '7309348405:AAEJmW2iw5zLkbhuEFg0kMlQIxyFpcEaZ0M'; // Bot token
 const WEB_LINK = 'https://66ab2fa429d0439148525d4d--techirrapp.netlify.app/'; // Web uygulamanızın URL'si
 
 const bot = new Telegraf(BOT_TOKEN);
 
+// Başlatma komutu
 bot.start((ctx) => {
   ctx.reply('Welcome! Use the button below to open the web app.', {
     reply_markup: {
@@ -16,14 +17,13 @@ bot.start((ctx) => {
   });
 });
 
-bot.launch();
-
+// Netlify fonksiyonu
 exports.handler = async function(event, context) {
   try {
     await bot.handleUpdate(JSON.parse(event.body));
     return { statusCode: 200, body: 'Update handled' };
   } catch (err) {
-    console.error('Error handling update', err);
+    console.error('Güncelleme işlenirken hata oluştu', err);
     return { statusCode: 500, body: 'Internal Server Error' };
   }
 };
