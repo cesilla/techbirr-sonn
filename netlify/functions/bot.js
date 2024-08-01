b847c1bd298dc3aba3373226afb2ec2378b24785
Elif Sude Cesur <elifsudecesur4@gmail.com>
2024-08-01 16:45:19 +0300

    Initial commit

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -1,10 +1,10 @@
 const { Telegraf } = require('telegraf');
-const BOT_TOKEN = '7309348405:AAEJmW2iw5zLkbhuEFg0kMlQIxyFpcEaZ0M'; // Bot token
-const WEB_LINK = 'https://66ab2fa429d0439148525d4d--techirrapp.netlify.app/'; // Web uygulamanızın URL'si
+const BOT_TOKEN = '7309348405:AAEJmW2iw5zLkbhuEFg0kMlQIxyFpcEaZ0M';
+const WEB_LINK = 'https://66ab2fa429d0439148525d4d--techirrapp.netlify.app/';
 
 const bot = new Telegraf(BOT_TOKEN);
 
-// Başlatma komutu
+// Basit bir komut
 bot.start((ctx) => {
   ctx.reply('Welcome! Use the button below to open the web app.', {
     reply_markup: {
@@ -20,10 +20,22 @@ bot.start((ctx) => {
 // Netlify fonksiyonu
 exports.handler = async function(event, context) {
   try {
-    await bot.handleUpdate(JSON.parse(event.body));
+    console.log('Event:', JSON.stringify(event, null, 2)); // Gelen isteği detaylı olarak loglayın
+
+    if (!event.body) {
+      console.error('Boş istek gövdesi');
+      return { statusCode: 400, body: 'Bad Request: Empty body' };
+    }
+
+    // İstek gövdesinin JSON formatında olduğundan emin olun
+    const update = JSON.parse(event.body);
+    console.log('Update:', update); // JSON parse edilen veriyi loglayın
+
+    await bot.handleUpdate(update);
+
     return { statusCode: 200, body: 'Update handled' };
   } catch (err) {
-    console.error('Güncelleme işlenirken hata oluştu', err);
+    console.error('Güncelleme işlenirken hata oluştu:', err);
     return { statusCode: 500, body: 'Internal Server Error' };
   }
 };
