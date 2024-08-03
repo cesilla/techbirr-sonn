a8b74fe391bc2658c85f70a805bf97d54cd91961
Elif Sude Cesur <elifsudecesur86@gmail.com>
2024-08-03 16:28:11 +0300

    Cüzdan bağlama işlemlerini güncelleme ve hata düzeltmeleri

--- front2/src/babel.config.js
+++ front2/src/babel.config.js
@@ -1,5 +1,10 @@
-{
-    "presets": ["react-app"],
-    "plugins": ["@babel/plugin-proposal-private-property-in-object"]
-  }
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-private-property-in-object'
+    ]
+  };
   
